51d3476bbac050cffdf61f6ce861799453018f59
tehshaw <25413002+tehshaw@users.noreply.github.com>
2021-09-28 10:19:57 -0400

    complete scripting and comments

---
+++ Assets/JS/schedule.js
@@ -0,0 +1,54 @@
+class schedule {
+
+    static dayOfWeek = moment().format("dddd");;
+    static date = moment().format("MMMM DD, YYYY");;
+    static todaysInfo;
+
+    constructor(theDate, theDay){
+        this.date = theDate;
+        this.dayOfWeek = theDay;
+        this.todaysInfo = [
+        {
+            hour: 9,
+            info: "test1"
+        },
+        {
+            hour: 10,
+            info: "test3"
+        },
+        {
+            hour: 11,
+            info: ""
+        },
+        {
+            hour: 12,
+            info: ""
+        },
+        {
+            hour: 13,
+            info: ""
+        },
+        {
+            hour: 14,
+            info: ""
+        },
+        {
+            hour: 15,
+            info: ""
+        },
+        {
+            hour: 16,
+            info: ""
+        },
+        {
+            hour: 17,
+            info: ""
+        }]
+    }
+
+    get theSchedule (){
+        return this.todaysInfo;
+    }
+
+
+}
--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,38 +1,48 @@
 var $timeContainer = $("#currentDay");
 var todayIS = moment().format("DDDD-YY");
-var todaySchedule = JSON.parse(localStorage.getItem(todayIS));
+var todaysSchedule;
 var $schduleDisplay = $(".container");
 var theTime = Number.parseInt(moment().format("k"));
+var scheduleInfo = new schedule();
 
-
-
-
-
-function scheduleCheck(){   
-
-    if(todaySchedule === null){
-        localStorage.setItem(todayIS, JSON.stringify(emptySchedule));
-        todaySchedule = emptySchedule;
+//get schedule from local storage
+//if there is no schedule for todays date it will make a new instance
+function getSchedule(){
+    todaysSchedule = JSON.parse(localStorage.getItem(todayIS));
+    if(todaysSchedule === null){
+        localStorage.setItem(todayIS, JSON.stringify(schedule.theSchedule));
+        todaysSchedule = schedule.theSchedule;
     }
 }
 
+//save the schdule back to local storage
+function setSchedule(){
+    localStorage.setItem(todayIS, JSON.stringify(todaysSchedule));
+}
+
+//build and display stored schedule
 function showSchedule(){
 
-    for(let x of todaySchedule){
+    //create a line in the scheduel for each hour
+    for(let x of todaysSchedule){
         var theHour = x.hour;
         var elID = "info-" + theHour;
+
+        //create containing div and classes
         var $divEL = $("<div>")
         $divEL.addClass("input-group row");
 
+        //create label and assocaited attr
         var $labelEL = $("<label>");
         $labelEL.attr({
             for: elID,
-            class: "input-group-text hour"
+            class: "input-group-text hour col-1"
         });
         theHour < 12 ? $labelEL.text(theHour +"AM") :
         theHour === 12 ? $labelEL.text("Noon") :
         $labelEL.text(theHour-12 + "PM");
 
+        //create textarea and asosciated arrt
         var $textEL = $("<textarea>");
         $textEL.attr({
             id: elID,
@@ -40,28 +50,49 @@ function showSchedule(){
         });
         $textEL.text(x.info);
 
+        //color text area based on time of day
+        //grey is hours past, red is current hour, green are hours not yet past
         console.log(theHour > theTime);
         theHour < theTime ? $textEL.addClass("form-control past") :
         theHour === theTime ? $textEL.addClass("form-control present") :
         $textEL.addClass("form-control future");
 
-        
-
+        //create button and assocaited attr
         var $buttonEL = $("<button>")
         $buttonEL.attr({
-            class: "btn saveBtn",
+            class: "btn saveBtn col-1",
             id: x.hour
         });
+        //add icon to button
         $buttonEL.append("<i class='fas fa-file'>");
 
+        //add all elements to the div
         $divEL.append($labelEL, [$textEL, $buttonEL]);
-
+        //then add the div to the site
         $schduleDisplay.append($divEL);
     }
 }
 
+//listen for any save button on the screen to be selected
+//will save all changes in all fields to local
+$schduleDisplay.on("click", ".saveBtn", function(){
 
+    //get all text areas (these contain the info for each hour)
+    var todaysEvents = $("div").find("textarea");
+    
+    //step through and write what ever is in the text fields
+    //into the clone of the local date in "todaysSchedule"
+    //then write back to local
+    for(let x = 0; x < todaysEvents.length; x++)
+    {
+        todaysSchedule[x].info = todaysEvents[x].value;
+    }
+    setSchedule();
+
+});
+
+//show todays date at the top of the screen
 $timeContainer.text(moment().format("dddd, MMMM Do") );
 
-scheduleCheck();
+getSchedule();
 showSchedule();
